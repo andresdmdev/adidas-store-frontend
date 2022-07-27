4c734ff409fbf59a761eb668f7fb561ea9e966d5
Andres Marquez <andresdmf55@gmail.com>
2022-07-26 20:56:51 -0500

    Adding routing to main files

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,35 @@
+import { useSelector } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
+import { showError } from '../services/slices/productsSlice'
 import './App.css'
 import Header from './components/header/Header'
-import Main from './components/main/Main'
+import MainSections from './components/main/MainSections'
 import Menu from './components/menu/Menu'
 
 function App() {
-  
+
+  const error = useSelector(showError)
 
   return (
-    <div className="App">
-      <Menu />
+    <div className='App'>
+      <>
+        <Routes>
+          <Route path='*' element={<Menu />} />
+        </Routes>
+      </>
       <div className='App_section'>
-        <Header />
-        <Main />
+        <Routes>
+          <Route path='*' element={<Header />} />
+        </Routes>
+        <div className="section_main">
+          { error ? 
+            <>
+              <h2>{error}</h2>
+              <p>Please, search again</p>
+            </> : 
+            <MainSections />
+          }
+        </div>
       </div>
     </div>
   )
