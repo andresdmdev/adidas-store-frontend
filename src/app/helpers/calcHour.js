2d75b52968fd25d813b20cb6c1bd6c1f0bb40ef9
Andres Marquez <andresdmf55@gmail.com>
2022-07-30 12:23:25 -0500

    Fixed logic of helper

--- src/app/helpers/calcHour.js
+++ src/app/helpers/calcHour.js
@@ -7,11 +7,11 @@ function hours(){
 
   let grettings
 
-  if(hour > 4 && hour < 12){
+  if(hour > 3 && hour < 12){
     grettings = 'Good morning' 
-  } else if(hour > 12 && hour < 18){
+  } else if(hour > 11 && hour < 18){
     grettings = 'Good afternoon' 
-  } else if(hour > 18 && hour < 22){
+  } else if(hour > 17 && hour < 22){
     grettings = 'Good evening' 
   } else {
     grettings = 'Good nigth' 
